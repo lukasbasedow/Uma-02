70990e60e2dd105e6bc6e844d5822daaa351b5b5
shou-majaro <shou@shou.moe>
2021-10-09 20:52:31 +0200

    histogram berechnen

--- main.py
+++ main.py
@@ -5,15 +5,28 @@ from numpy.random import randint
 
 number = []
 size = sys.getsizeof(number)
-counter = 0
+klassenbreite = 40
+bin = 256 / klassenbreite
+bin = int(bin)
+bins = [0] * bin
+fx = []
 
 while size < 16*1048576:
     value = randint(0, 255)
     size = sys.getsizeof(number)
-    counter = counter +1
     number.append(value)
 
-print(counter)
+for x in range(0,bin):
+    for y in number:
+        if y < klassenbreite*x:
+           bins[x] = bins[x]+1
+        if x == 0 & y < klassenbreite:
+            bins[x] = bins[x] + 1
+
+for x in bins:
+    fx.append(x / (len(number) * klassenbreite))
+
 print(sys.getsizeof(number))
-plt.hist(number, bins=25)
+print(fx)
+plt.hist(number, bins=bin)
 plt.show()

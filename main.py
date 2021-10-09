7f7a4d2c4a739856ee86f9d7e05a27f486bff778
shou-majaro <shou@shou.moe>
2021-10-09 21:42:52 +0200

    histogram fix, jetzt mit richtigen zahlen

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ bin = 256 / klassenbreite
 bin = int(bin)
 bins = [0] * bin
 fx = []
+str4 = str(klassenbreite)
+plot = ["0-"+str4]
 
 while size < 16*1048576:
     value = randint(0, 255)
@@ -17,16 +19,22 @@ while size < 16*1048576:
     number.append(value)
 
 for x in range(0,bin):
+    if x > 0:
+        str1 = str(klassenbreite*(x+1)-klassenbreite)
+        str2 = str(klassenbreite*(x+1))
+        str3 = '%s-%s' % (str1, str2)
+        plot.append(str3)
+
     for y in number:
-        if y < klassenbreite*x:
+        if y in range(klassenbreite*(x+1)-klassenbreite,klassenbreite*(x+1)):
            bins[x] = bins[x]+1
-        if x == 0 & y < klassenbreite:
-            bins[x] = bins[x] + 1
 
 for x in bins:
     fx.append(x / (len(number) * klassenbreite))
 
-print(sys.getsizeof(number))
-print(fx)
+print("Size of list: ",sys.getsizeof(number))
+print("F(x) bei Klassenbreite: ", klassenbreite," und Variationsbereie: ", bin, fx)
 plt.hist(number, bins=bin)
+plt.show()
+plt.bar(plot,fx)
 plt.show()

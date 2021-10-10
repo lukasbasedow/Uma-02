0b9606bb4cb84539d3cacd37ccc4fdf6038efdcb
shou-majaro <shou@shou.moe>
2021-10-10 20:02:58 +0200

    add Kommentare

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import sys
 import matplotlib.pyplot as plt
 from numpy.random import randint
 
+#Klassenbreite Parameter
+klassenbreite = 40
+
 number = []
 size = sys.getsizeof(number)
-klassenbreite = 40
 bin = 256 / klassenbreite
 bin = int(bin)
 bins = [0] * bin
@@ -13,13 +15,16 @@ fx = []
 str4 = str(klassenbreite)
 plot = ["0-"+str4]
 
+#generieren der Daten
 while size < 16*1048576:
     value = randint(0, 255)
     size = sys.getsizeof(number)
     number.append(value)
 
+#binning
 for x in range(0,bin):
     if x > 0:
+        #x beschriftung generieren
         str1 = str(klassenbreite*(x+1)-klassenbreite)
         str2 = str(klassenbreite*(x+1))
         str3 = '%s-%s' % (str1, str2)
@@ -29,9 +34,11 @@ for x in range(0,bin):
         if y in range(klassenbreite*(x+1)-klassenbreite,klassenbreite*(x+1)):
            bins[x] = bins[x]+1
 
+#errechnen von h
 for x in bins:
     fx.append(x / (len(number) * klassenbreite))
 
+#zeichnen des Histograms
 print("Size of list: ",sys.getsizeof(number))
 print("F(x) bei Klassenbreite: ", klassenbreite," und Variationsbereie: ", bin, fx)
 plt.hist(number, bins=bin)
